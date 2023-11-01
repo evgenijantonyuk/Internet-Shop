668f5caa5786733a06f6d0f16001204c801c97d5
Evgenij Antonyuk <96491514+evgenijantonyuk@users.noreply.github.com>
2023-11-01 23:52:58 +0700

    shop

---
+++ js/calcCartPrice.js
@@ -0,0 +1,19 @@
+function calcCartPriceAndDelivery() {
+	const cartWrapper = document.querySelector('.cart-wrapper');
+	const priceElements = cartWrapper.querySelectorAll('.price__currency');
+	const totalPriceEl = document.querySelector('.total-price');
+	
+	// Общая стоимость товаров
+	let priceTotal = 0;
+	
+	// Обходим все блоки с ценами в корзине
+	priceElements.forEach(function (item) {
+		// Находим количество товара
+		const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
+		// Добавляем стоимость товара в общую стоимость (кол-во * цену)
+		priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
+	});
+	
+	// Отображаем цену на странице
+	totalPriceEl.innerText = priceTotal;
+}
---
+++ js/clearCounter.js
@@ -0,0 +1,13 @@
+
+
+// let btnDel = document.querySelector('.btn-delete')
+//
+//
+// btnDel.onclick = () => {
+//     const orderDel = () => {
+//         document.querySelector('.col-md-6')
+//         onclick.classList.add('none')
+//     }
+//
+//     console.log('orderDel')
+// }
